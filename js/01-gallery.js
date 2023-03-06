d6761d2276e33555c5546d4c08019791683efc4c
KacperMadajewski <kacpermadajewski@gmail.com>
2023-03-06 19:24:58 +0100

    Zrobione zadanie 1
    Zadanie 1 zrobione ale dla większości zdjęć, po kliknięciu chce pobierać, a nie od razu otworzyć większe zdjęcie

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
 
 const gallery = document.querySelector(".gallery");
 
+galleryItems.forEach((item) => {
+  const galletyItem = `<div class="gallery__item">
+    <a class="gallery__link" href="${item.original}">
+    <img class="gallery__image"
+    src="${item.preview}" 
+    data-source="${item.original}" 
+    alt="${item.description}" /></a></div>`;
+  gallery.insertAdjacentHTML("beforeend", galletyItem);
+});
+const instance = basicLightbox.create(
+  (document.querySelector(".gallery").onclick = (event) => {
+    event.preventDefault();
+    instance.create(
+      `
+        <img width="1400" height="900" src="${event.target.dataset.source}">
+    `
+    );
+    instance.show();
+  })
+);
